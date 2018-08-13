9cd5325dcfb6efd183dcbfd77fc6e0fd47c17a59
Adrià Pallejà <adria.palleja@gmail.com>
2018-08-13 16:44:21 +0200

    async stuff implemented

--- async.js
+++ async.js
@@ -6,18 +6,29 @@ const randomNumber = () => {
 }
 
 // 1. Make it wait for 1 sec. with `setTimeout` and log it on main function
-const timeoutRandomNumber = () => {
-
+const timeoutRandomNumber = (cb) => {
+  setTimeout(() => {
+    try {
+      cb(null,randomNumber());
+    } catch (err) {
+      cb(err);
+    }
+  },1000);
 }
 
 // 2. Now wrap the timeout version to work with promises
 const promiseRandomNumber = () => {
-
+  return new Promise(function(resolve,reject) { 
+    timeoutRandomNumber((err,num) => {
+      if (err) reject(err);
+      else resolve(num);
+    })
+  });
 }
 
 // 3. Finally, code a final version with async await.
-const asyncRandomNumber = () => {
-
+const asyncRandomNumber = async () => {
+  return await promiseRandomNumber();
 }
 
 const rangedRandomNumber = (base, min, max) => {
@@ -25,10 +36,22 @@ const rangedRandomNumber = (base, min, max) => {
 }
 
 const main = () => {
-  const rnd = randomNumber()
-  console.log(rangedRandomNumber(rnd, 14, 42));
+  const rnd = randomNumber();
+  console.log('Random ',rangedRandomNumber(rnd, 14, 42));
 
   // log rnd…
+  timeoutRandomNumber((err,rnd)=>{
+    if (err) throw err;
+    else console.log('Callback ',rangedRandomNumber(rnd, 14, 42));
+  });
+  promiseRandomNumber()
+    .then(rnd => console.log('Promise ',rangedRandomNumber(rnd, 14, 42)))
+    .catch(err => console.log(err));
+  asyncRandomNumber()
+    .then(rnd => console.log('Async ',rangedRandomNumber(rnd, 14, 42)))
+    .catch(err => console.log(err));
 }
 
 main();
+
+module.exports = main;
